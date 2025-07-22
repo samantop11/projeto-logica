c8f458f3601fdd81474fa3026c64d55b5c4dd259
samantop11 <samantha.santos.alves@escola.pr.gov.br>
2025-07-22 08:51:16 -0300

    Update SamanthaeTaissa .c

--- SamanthaeTaissa .c
+++ SamanthaeTaissa .c
@@ -1,34 +1,10 @@
+#incluir <stdio.h>
 
-#include <stdio.h>
-
-int main()
+int principal()
 {
-    float valor_emprestimo;
-    float taxa_juros_anual;
-    float montante;
-    float prestacao;
-    int num_parcelas;
-    float taxa_juros_mensal;
-     
-     printf("digite valor do emprestimo: ");
-     scanf("%f",&valor_emprestimo);
-     
-     printf("digite taxa juros anual: ");
-     scanf("%f", &taxa_juros_anual);
-     
-     printf("digite numero de parcelas: ");
-     scanf("%d", &num_parcelas);
-     
-     //calculo
-     
-     taxa_juros_mensal=taxa_juros_anual /12;
-     
-     
-     float juros_total=valor_emprestimo*(taxa_juros_mensal/100);
-     float valor_final=valor_emprestimo+juros_total;
-     prestacao=valor_final/num_parcelas;
-     
-     printf("o valor de cada parcela é:%.2f\n", prestacao);
-
-    return 0;
-}
+flutuador calor_emprestimo;
+flutuador taxa_juros_anual;
+flutuador montante;
+flutuador prestação;
+int num_parcelas;
+flutuador taxa_juros
